d348d452e2331dbd8563d46b4d207822ce1ac334
Gourav <gouravrao107@gmai.com>
2026-06-21 08:20:35 +0530

    suday21

---
+++ js/custom.js
@@ -0,0 +1,85 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+    // CUSTOM LINK
+    $('.custom-link').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 10;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+  }
+});
+    
+  })(window.jQuery);
+
+  function searchImages(){
+
+    let input = document.getElementById("searchInput").value;
+
+    if(input.trim() === ""){
+        alert("Please enter something!");
+    }else{
+        alert("Searching for: " + input);
+    }
+}
+
+// Hover Play Videos
+
+const videos = document.querySelectorAll(".hover-video");
+
+videos.forEach(video => {
+
+    video.addEventListener("mouseenter", () => {
+        video.play();
+    });
+
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+
+});
+
+function searchImages() {
+
+    let input = document.getElementById("searchInput").value.toLowerCase();
+
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card) {
+
+        let titleElement = card.querySelector("h3");
+
+        if (!titleElement) return;
+
+        let title = titleElement.textContent.toLowerCase();
+
+        if (title.includes(input)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+}
+
+
+
